d3a861f00d83f3bc8b2f5390c964f8bf5c22b44b
Mwamiq <wamiq.wasi@vrcreators.co>
2022-06-17 16:51:19 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [inputValue, setInputValue] = useState("")
+
+  const [todoList, setTodoList] = useState([])
+
+  const [btnText, setBtnText] = useState("Add")
+
+  const [updateFlag, setUpdateFlag] = useState(null)
+
+  const addItem = () => {
+
+    if (updateFlag !== null) {
+      return updateItem()
+    }
+    setTodoList(prev=>[...prev,inputValue])
+    setInputValue("")
+  }
+  const deleteItem = (indexValue) => {
+    todoList.splice(indexValue, 1);
+    setTodoList(prev=>[...prev])
+  }
+
+  const editItem = (item, indexValue) => {
+    setInputValue(item)
+    setBtnText("Update")
+    setUpdateFlag(indexValue)
+  }
+
+  const updateItem = () => {
+    setInputValue(updateFlag)
+    todoList.splice(updateFlag, 1, inputValue);
+    setBtnText("Add");
+    setUpdateFlag(null)
+    setInputValue("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="main-container">
+        <h1>Todo In React</h1>
+        <div className="input-field">
+          <input type="text" value={inputValue} placeholder="Add Item" onChange={e => setInputValue(e.target.value)}/>
+          <button type="button" onClick={addItem}>{btnText}</button>
+        </div>
+        <ul>
+        {todoList.map((item, index) => {
+          return <li key={item}>{item} <button type='button' onClick={() => editItem(item, index)}>Edit</button><button type='button' onClick={() => deleteItem(index)}>Delete</button></li> 
+        })}
+        </ul>
+      </div>
   );
 }
 
