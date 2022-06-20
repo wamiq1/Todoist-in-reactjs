1b06da61481a3bfb048b06b9b8610e0793d15716
Mwamiq <wamiq.wasi@vrcreators.co>
2022-06-20 15:52:04 +0500

    Update Status and edit item background Color

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  const [inputValue, setInputValue] = useState("")
+  const [inputValue, setInputValue] = useState("");
 
-  const [todoList, setTodoList] = useState([])
+  const [todoList, setTodoList] = useState([]);
 
-  const [btnText, setBtnText] = useState("Add")
+  const [btnText, setBtnText] = useState("Add");
 
-  const [updateFlag, setUpdateFlag] = useState(null)
+  const [updateFlag, setUpdateFlag] = useState(null);
+
+  const [isComplete, setIsComplete] = useState(null)
+
+  const [isDisabled, setIsDisabled] = useState(false)
 
   const addItem = () => {
 
@@ -20,13 +24,14 @@ function App() {
   }
   const deleteItem = (indexValue) => {
     todoList.splice(indexValue, 1);
-    setTodoList(prev=>[...prev])
+    setTodoList(prev=>[...prev]);
   }
 
   const editItem = (item, indexValue) => {
-    setInputValue(item)
-    setBtnText("Update")
-    setUpdateFlag(indexValue)
+    setInputValue(item);
+    setBtnText("Update");
+    setUpdateFlag(indexValue);
+    setIsDisabled(true);
   }
 
   const updateItem = () => {
@@ -34,7 +39,13 @@ function App() {
     todoList.splice(updateFlag, 1, inputValue);
     setBtnText("Add");
     setUpdateFlag(null)
+    setIsDisabled(false);
     setInputValue("")
+  }
+
+  const updateStatus = (indexValue) => {
+    setIsComplete(indexValue);
+    setIsDisabled(true);
   }
   return (
       <div className="main-container">
@@ -45,7 +56,20 @@ function App() {
         </div>
         <ul>
         {todoList.map((item, index) => {
-          return <li key={item}>{item} <button type='button' onClick={() => editItem(item, index)}>Edit</button><button type='button' onClick={() => deleteItem(index)}>Delete</button></li> 
+          return <li key={item} 
+                    style={updateFlag === index ? {background: 'white'} : {}}>
+                      <span style={isComplete === index ? {textDecoration: 'line-through'} : {}}>{item}</span> 
+                    <button onClick={() => updateStatus(index)} 
+                            disabled={isDisabled || updateFlag === index} 
+                            style={(isDisabled && updateFlag === index) ? {background: '#c414fda1', border: '#c414fda1'} : {}}>Complete
+                    </button>
+                    <button onClick={() => editItem(item, index)}>Edit
+                    </button>
+                    <button onClick={() => deleteItem(index)}
+                            disabled={isDisabled && updateFlag === index} 
+                            style={(isDisabled && updateFlag === index) ? {background: '#c414fda1', border: '#c414fda1'} : {}}>Delete
+                    </button>
+                  </li> 
         })}
         </ul>
       </div>
